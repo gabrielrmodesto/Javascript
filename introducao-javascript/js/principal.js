ba6e4dc9702cde41f10ed5941477628aa706f7f4
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-07-05 17:36:42 -0300

    Funcao do botao implementada para adicionar uma linha na tabela

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -37,6 +37,41 @@ for (var i = 0; i < pacientes.length; i++) {
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
 	event.preventDefault();
-    alert("Oi, cliquei no botão.");
+	//seleciono o formulario
+    var form = document.querySelector("#form-adiciona");
+    //valores dos inputs atraves de name
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    //criando a tr
+    var pacienteTr = document.createElement("tr");
+    //criando as td
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    //fazendo com que as td tenham conteudo
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    //criando o apendice entre tr e td
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    //pegando o tbody para ter o tr ligado a ele
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+    var campos = document.querySelectorAll("input");
+    for(i=0; i<campos.length; i++){
+    	var campo = campos[i];
+    	campo.value = "";
+    }
+    
+
 });
 //console.log(imc);
