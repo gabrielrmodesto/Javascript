37af092dda8ed3fe1d9fc43ec577ebf3df51f516
GabrielRM <gabrielrmodesto5@gmail.com>
2018-07-06 11:37:29 -0300

    Codigo separado em arquivo para melhor adequação

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -1,39 +1,3 @@
-//capitulo 2
-//Video de operadores logicos 
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
-
-var pacientes = document.querySelectorAll(".paciente");
-for (var i = 0; i < pacientes.length; i++) {
-	var paciente = pacientes[i];
-
-	var tdPeso = paciente.querySelector(".info-peso");
-	var peso = tdPeso.textContent;
-	var tdAltura = paciente.querySelector(".info-altura");
-	var altura = tdAltura.textContent;
-	var tdImc = paciente.querySelector(".info-imc");
-	var pesoValido = true;
-	var alturaValido = true;
-
-	if(peso<=0 || peso>=1000){
-		//console.log("Peso invalido");
-		pesoValido = false;
-		tdImc.textContent = "Peso inválido!";
-		paciente.classList.add("paciente-invalido");
-	}
-	if(altura<=0 || altura>=3.00){
-	    //console.log("Altura inválida");
-	    alturaValido = false;
-	    tdImc.textContent = "Altura inválida!";
-	    paciente.classList.add("paciente-invalido");
-	}
-
-	if(pesoValido && alturaValido){
-		var imc = peso / (altura*altura);
-		tdImc.textContent = imc.toFixed(2);
-	}
-}
-
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
 	event.preventDefault();
@@ -74,4 +38,3 @@ botaoAdicionar.addEventListener("click", function(event){
     
 
 });
-//console.log(imc);
