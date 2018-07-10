fff264f7f9bbe821be9db083da5745ece455e019
GabrielRM <gabrielrmodesto5@gmail.com>
2018-07-10 17:28:44 -0300

    Função de remover o paciente atraves de dois cliques

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -58,12 +58,12 @@ function montaTd(dado,classe){
 
 function validaPaciente(paciente){
     var erros = [];
-    if(paciente.nome.length == 0) erros.push("O nome nao pode ser em branco");
-    if(paciente.gordura.length == 0) erros.push("A gordura nao pode ser em branco");
-    if(paciente.peso.length == 0) erros.push("O peso nao pode ser em branco");
-    if(paciente.altura.length == 0) erros.push("A altura nao pode ser em branco");
-    if(!validaPeso(paciente.peso)) erros.push("Peso invalido");
-    if(!validaAltura(paciente.altura)) erros.push("Altura invalida");
+    if(paciente.nome.length == 0) erros.push("O nome não pode ser em branco");
+    if(paciente.gordura.length == 0) erros.push("A gordura não pode ser em branco");
+    if(paciente.peso.length == 0) erros.push("O peso não pode ser em branco");
+    if(paciente.altura.length == 0) erros.push("A altura não pode ser em branco");
+    if(!validaPeso(paciente.peso)) erros.push("Peso inválido");
+    if(!validaAltura(paciente.altura)) erros.push("Altura inválida");
     return erros;
 }
 
