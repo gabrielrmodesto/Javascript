7918cdb1d25828555b9529252102dd23e311a75b
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-07-10 11:14:06 -0300

    Funcao de exibir mensagem, array de erros com mensagem em caso de ficar em branco o campo

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -7,10 +7,10 @@ botaoAdicionar.addEventListener("click", function(event){
     var paciente = obtemPacienteDoFormulario(form);
     //chamando a funcao de montar as tr
     var pacienteTr = montaTr(paciente);
-    var erro = validaPaciente(paciente);
-    if(erro.length > 0){
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erro;
+    var erros = validaPaciente(paciente);
+    console.log(erros);
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
         return;
     }
     //pegando o tbody para ter o tr ligado a ele
@@ -18,6 +18,8 @@ botaoAdicionar.addEventListener("click", function(event){
     tabela.appendChild(pacienteTr); 
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
 
 function obtemPacienteDoFormulario(form){
@@ -55,10 +57,22 @@ function montaTd(dado,classe){
 }
 
 function validaPaciente(paciente){
-    if(validaPeso(paciente.peso)){
-        return "";
-    }
-    else{
-        return "Paciente invalido";
-    }
+    var erros = [];
+    if(paciente.nome.length == 0) erros.push("O nome nao pode ser em branco");
+    if(paciente.gordura.length == 0) erros.push("A gordura nao pode ser em branco");
+    if(paciente.peso.length == 0) erros.push("O peso nao pode ser em branco");
+    if(paciente.altura.length == 0) erros.push("A altura nao pode ser em branco");
+    if(!validaPeso(paciente.peso)) erros.push("Peso invalido");
+    if(!validaAltura(paciente.altura)) erros.push("Altura invalida");
+    return erros;
+}
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML= "";
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
