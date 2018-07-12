362cd9ad4386d4d749ba3e1ce128ce7bae5e4544
GabrielRM <gabrielrmodesto5@gmail.com>
2018-07-12 17:49:01 -0300

    Função de remover com dois cliques até os novos

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -1,7 +1,16 @@
+// var pacientes = document.querySelectorAll(".paciente");
+
+// pacientes.forEach(function(paciente){
+// 	paciente.addEventListener("dblclick", function(){
+// 		this.remove();
+// 	});
+// });
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-	paciente.addEventListener("dblclick", function(){
-		this.remove();
-	});
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+	var alvo = event.target;
+	var paiDoAlvo = alvo.parentNode;
+	paiDoAlvo.remove();
 });
